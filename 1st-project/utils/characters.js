2357421674731a98b41aea4bac66dd9be1c2c018
Angel Garcia <cesar.cfeteit@gmail.com>
2023-08-25 14:57:23 -0600

    feat: avance con el llamado de la API

---
+++ 1st-project/utils/characters.js
@@ -0,0 +1,15 @@
+const request = require('postman-request');
+
+const getCharacters = callback => {
+    const url = 'https://finalspaceapi.com/api/v0/character/';
+
+request({url, json: true }, (error, response) => {
+    if(error || response.body.error) {
+        console.log('Unable to connect to the server');
+    }else {
+        console.log(response.body)
+    }
+})
+}
+
+module.exports = getCharacters;
