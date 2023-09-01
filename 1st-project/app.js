161e6e8dd55f8c6560a238d239e689cc0cbc3661
Angel Garcia <cesar.cfeteit@gmail.com>
2023-09-01 17:44:26 -0600

    Reboot of the repository

--- 1st-project/app.js
+++
@@ -1,9 +0,0 @@
-const getCharacters = require('./utils/characters');
-
-getCharacters((error, data) => {
-    if(error){
-        return console.log(error)
-    } else {
-        console.log(data)
-    }
-})
